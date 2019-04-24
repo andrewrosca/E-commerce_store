5b86a7aabeb113ce9be4c2ca95832e9302ae96f9
Andrew <andrewrosca7@gmail.com>
2019-04-23 22:37:29 -0700

    Added php functionality to pull featured products, categories, and products from the database

--- public/item.php
+++ public/item.php
@@ -26,14 +26,14 @@ while($row = fetch_array($query)):
 <div class="row">
 
     <div class="col-md-7">
-       <img class="img-responsive" src="http://placehold.it/700x600" alt="">
+       <img class="img-responsive" src="<?php echo $row['product_image']; ?>" alt="">
 
     </div>
 
     <div class="col-md-5">
 
         <div class="thumbnail"
-
+      
     <div class="caption-full">
         <h4><a href="#"><?php echo $row['product_title']; ?></a> </h4>
         <hr>
@@ -51,7 +51,7 @@ while($row = fetch_array($query)):
         </p>
     </div>
           
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p><?php echo $row['short_desc'];?></p>
 
    
     <form action="">
--- resources/functions.php
+++ resources/functions.php
@@ -19,13 +19,13 @@ function confirm($result) {
     }
 }
 
-
+// Escapes special characters in a string for use in an SQL statement
 function escape_string($string) {
     global $connection;
     return mysqli_real_escape_string($connection, $string); 
 }
 
-
+// Fetches a result row as an associative, a numeric array, or both
 function fetch_array($result) {
     return mysqli_fetch_array($result); 
 }
@@ -70,12 +70,65 @@ function get_categories() {
     $query = query("SELECT * FROM categories");
     confirm($query); 
     while($row = fetch_array($query)) {
-        $category = <<<DELIMETER2
+        $category = <<<DELIMETER
         <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
-DELIMETER2;
+DELIMETER;
         echo $category; 
     }
 }
+
+
+// function get_features() {
+//     $query = query("SELECT * FROM products WHERE product_category_id = " . escape_string($_GET['id']) . " ");
+//     confirm($query);
+    
+//     while($row = fetch_array($query)) {
+//         $feature = <<<DELIMETER
+//         <div class="col-sm-4 col-lg-4 col-md-4">
+//             <div class="thumbnail">
+//                 <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
+//                 <div class="caption">
+//                     <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+//                     <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+//                     </h4>
+//                     <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+//                     <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to Cart</a>
+//                 </div>
+//             </div>
+//          </div>
+
+// DELIMETER;
+//         echo $feature;
+//     }
+
+// }
+
+
+
+function get_features() {
+    $query = query("SELECT * FROM products WHERE product_category_id = " . escape_string($_GET['id']) . " "); 
+    confirm($query); 
+
+    while($row = fetch_array($query)) {
+        $feature = <<<DELIMETER
+        <div class="col-md-3 col-sm-6 hero-feature">
+            <div class="thumbnail">
+                <img src="{$row['product_image']}" alt="">
+                <div class="caption">
+                    <h3>{$row['product_title']}</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    <p>
+                    <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                    </p> 
+                </div>
+            </div>
+        </div>
+
+DELIMETER;
+        echo $feature; 
+    }
+}
+
 
 
 
